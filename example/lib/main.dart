4ecd34b53b5598697f5c9377fb0e1bee15d44ea4
yinglitang <tang4595@163.com>
2021-07-07 11:51:43 +0800

    fix依赖问题

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,137 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_contact_example/people_list_page.dart';
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
-import 'package:logging/logging.dart';
-import 'package:logging_config/logging_config.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'dart:async';
 
-import 'add_contact_page.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_contact/flutter_contact.dart';
 
 void main() {
-  configureLogging(LogConfig.root(Level.INFO));
-  runApp(ContactsExampleApp());
+  runApp(MyApp());
 }
 
-class ContactsExampleApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: <String, WidgetBuilder>{
-        '/': (context) => const HomePage(),
-        '/add': (BuildContext context) => AddContactPage(),
-        '/contactsList': (BuildContext context) => PeopleListPage(),
-      },
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
+class _MyAppState extends State<MyApp> {
+  String _platformVersion = 'Unknown';
 
-  const HomePage();
-}
-
-class _HomePageState extends State<HomePage> {
-  bool _hasPermission;
   @override
   void initState() {
     super.initState();
-    _askPermissions();
+    initPlatformState();
   }
 
-  Future<void> _askPermissions() async {
-    PermissionStatus permissionStatus;
-    while (permissionStatus != PermissionStatus.granted) {
-      try {
-        permissionStatus = await _getContactPermission();
-        if (permissionStatus != PermissionStatus.granted) {
-          _hasPermission = false;
-          _handleInvalidPermissions(permissionStatus);
-        } else {
-          _hasPermission = true;
-        }
-      } catch (e) {
-        if (await showPlatformDialog(
-            context: context,
-            builder: (context) {
-              return PlatformAlertDialog(
-                title: Text('Contact Permissions'),
-                content: Text(
-                    'We are having problems retrieving permissions.  Would you like to '
-                    'open the app settings to fix?'),
-                actions: [
-                  PlatformDialogAction(
-                    child: Text('Close'),
-                    onPressed: () {
-                      Navigator.pop(context, false);
-                    },
-                  ),
-                  PlatformDialogAction(
-                    child: Text('Settings'),
-                    onPressed: () {
-                      Navigator.pop(context, true);
-                    },
-                  ),
-                ],
-              );
-            })) {
-          await openAppSettings();
-        }
-      }
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      platformVersion = await FlutterContact.platformVersion;
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
     }
 
-    await Navigator.of(context).pushReplacementNamed('/contactsList');
-  }
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
 
-  Future<PermissionStatus> _getContactPermission() async {
-    final status = await Permission.contacts.status;
-    if (!status.isGranted) {
-      final result = await Permission.contacts.request();
-      return result ?? PermissionStatus.undetermined;
-    } else {
-      return status;
-    }
-  }
-
-  void _handleInvalidPermissions(PermissionStatus permissionStatus) {
-    if (permissionStatus == PermissionStatus.denied) {
-      throw PlatformException(
-          code: 'PERMISSION_DENIED',
-          message: 'Access to location data denied',
-          details: null);
-    } else if (permissionStatus == PermissionStatus.restricted) {
-      throw PlatformException(
-          code: 'PERMISSION_DISABLED',
-          message: 'Location data is not available on device',
-          details: null);
-    }
+    setState(() {
+      _platformVersion = platformVersion;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Contacts Plugin Example')),
-      body: _hasPermission == null
-          ? Center(child: PlatformCircularProgressIndicator())
-          : SafeArea(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  RaisedButton(
-                    child: const Text('Contacts list'),
-                    onPressed: () =>
-                        Navigator.pushNamed(context, '/contactsList'),
-                  ),
-                  RaisedButton(
-                    child: const Text('Native Contacts picker'),
-                    onPressed: () =>
-                        Navigator.pushNamed(context, '/nativeContactPicker'),
-                  ),
-                ],
-              ),
-            ),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Text('Running on: $_platformVersion\n'),
+        ),
+      ),
     );
   }
 }
